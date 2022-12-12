900c53231e243f315dcdc2c72246ca36bd7b805c
TotskayaOV <olga.totskaya@gmail.com>
2022-12-12 21:29:37 +0700

    Added if-else

--- 01_HelloPython/hello.py
+++ 01_HelloPython/hello.py
@@ -1,1 +1,17 @@
-print('Hello World!')
+# print('Hello World!')
+
+#f = [1,2,3,4]
+#is_odd = not f[1] % 2 
+#print(is_odd)
+#is_odd2 = f[0] % 2 == 0
+#print(is_odd2)
+
+#Управляющие конструкции 
+#if, if-else
+
+a = int(input('a = '))
+b = int(input('b = '))
+if a > b:
+    print(a)
+else:
+    print(b)
