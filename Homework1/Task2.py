24a623f570f28feee2ce8a7ce2e5db72405ff338
TotskayaOV <olga.totskaya@gmail.com>
2022-12-13 15:57:07 +0700

    Created Homework1

---
+++ Homework1/Task2.py
@@ -0,0 +1,36 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def f (list_1):
+    result1 = not sum(list_1)
+    result2 = not list_1[0] and not list_1[1] and not list_1[2]
+    result = result1 == result2
+    return result
+
+predicators = [False, False, False]
+result = f(predicators)
+if result == True:
+    predicators = [True, False, False]
+    result = f(predicators)
+    if result == True:
+        predicators = [False, True, False]
+        result = f(predicators)
+        if result == True:
+            predicators = [False, False, True]
+            result = f(predicators)
+            if result == True:
+                predicators = [True, True, False]
+                result = f(predicators)
+                if result == True:
+                    predicators = [True, False, True]
+                    result = f(predicators)
+                    if result == True:
+                        predicators = [False, True, True]
+                        result = f(predicators)
+                        if result == True:
+                            predicators = [True, True, True]
+                            result = f(predicators)
+                            if result == True:
+                                print('Утверждение истинно')
+else:
+    print('Утверждение ложно')
+ 
