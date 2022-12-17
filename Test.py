68b75833e10a31d0ed50af621404b076ab85e066
TotskayaOV <olga.totskaya@gmail.com>
2022-12-18 01:22:07 +0700

    123

--- Test.py
+++ Test.py
@@ -37,16 +37,35 @@
 #     data.write('line1\n')
 #     data.write('line2\n')
 
-import readline
-position_num = 3
-path = 'file.txt'   # чтение данных из файла
-my_list = []
-with True:
-    i = path.readline()
-    if not i:
-        break
-    print(i)
+
+# position_num = 3
+# path = 'file.txt'   # чтение данных из файла
+# my_list = []
+# with True:
+#     i = path.read()
+#     if not i:
+#         break
+#     print(i)
    
+path = 'file.txt'   # чтение данных из файла
+data = open(path, 'r')
+my_list = []
+for line in data:
+    line = line.replace('\n', '')
+    my_list.append(line)
+data.close()
+str12 = ''.join(my_list)
+# for i in str12:
+#     if '\n' in str12:
+#         str12.remove('\n')
+#     else:
+#         break
+
+# my_list = my_list.replace('\n', '')
+  
+print(my_list)
+print(str12)
+
 # content = path.read
 # element_num = content(position_num)
 # data = open(path, 'r')
